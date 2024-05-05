c23d76c10150345e525e4e09b52045b8b647447a
UntestedEngineer <cpiehler2@gmail.com>
2024-05-05 12:34:25 -0400

    Update docker files for ubuntu 22.04 compatibility

--- docker-build.sh
+++ docker-build.sh
@@ -43,6 +43,15 @@ apt-get install -qy --no-install-recommends \
 echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 tryfail apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 06E85760C0A52C50
 
+# Add mongodb CE edition repository (mongodb is not included in > 22.04 default repositories)
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+# Install mongodb CE edition
+apt-get update
+apt-get install -qy --no-install-recommends \
+    mongodb-org-database
+
 if [ -d "/usr/local/docker/pre_build/$(dpkg --print-architecture)" ]; then
     find "/usr/local/docker/pre_build/$(dpkg --print-architecture)" -type f -exec '{}' \;
 fi
